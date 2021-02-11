9a0af231908bad1a8096d7557375af7ccc6a3022
Rafael H. Osorio <rafa.osorioa@gmail.com>
2021-02-11 12:31:47 -0500

    Finished announcements

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'announcements#index'
   # get '/announcements/new', to: 'announcements#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#index'
+  resources :announcements
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_234538) do
+ActiveRecord::Schema.define(version: 2021_02_11_135901) do
+
+  create_table "announcements", force: :cascade do |t|
+    t.string "announcement"
+    t.date "expiration"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_announcements_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", default: "", null: false
